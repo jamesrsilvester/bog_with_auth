8693e9509c8664b6e5ccf577053de69dde869200
sonoilconte <shammersinger@yahoo.com>
2017-08-23 13:43:11 -0700

    edit form and update working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -10,7 +10,7 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
+    # creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
@@ -18,14 +18,34 @@ class CreaturesController < ApplicationController
   end
 
   def show
-    creatureId = params[:id]
-    @creature = Creature.find_by(id: creatureId)
+    creature_id = params[:id]
+    @creature = Creature.find_by(id: creature_id)
   end
 
   def edit
-    creatureId = params[:id]
-    @creature = Creature.find_by(id: creatureId)
-    
+    creature_id = params[:id]
+    @creature = Creature.find_by(id: creature_id)
+  end
+
+  def update
+
+    # get the creature id from the params
+    creature_id = params[:id]
+    # use the creature_id to find the right creature in the DB
+    creature = Creature.find_by(id: creature_id)
+    # update the creature
+    creature.update_attributes(creature_params)
+    # redirect to show the updated creature
+    redirect_to creature_path(creature)
+    # could also be:
+    # redirect_to "/creatures/#{creature.id}"
+
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
